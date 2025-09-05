caaeffb4e4c162cfa4cb2525b74a0c5db01afe63
faizaa123 <f18610195@gmail.com>
2025-09-05 03:57:26 -0700

    js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let age ;
 age=14
 alert("I am" +age+" years old")
-// Q2
+git 
     let visits = localStorage.getItem("visitCount");
     if (visits === null) {
       visits = 0;
@@ -9,11 +9,11 @@ alert("I am" +age+" years old")
     visits++;
     localStorage.setItem("visitCount", visits);
     alert("You have visited this site " + visits + " times.");
-    // Q3
+    
 let birthyear = 2011
    document.write("My birth year is " + birthyear + "<br>");
      document.write("Data type of my declared variable is number" );
-    //    Q4
+    
     var visitorName = "John Doe";
     var productTitle = "T-shirt";
     var quantity = 5;
