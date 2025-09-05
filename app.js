82b14c2bc3e4b9fd4cb069260febb94d3b1db6d6
faizaa123 <f18610195@gmail.com>
2025-09-05 04:10:16 -0700

    js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-let age ;
-age=14
-alert("I am" +age+" years old")
-git 
+ // Age alert
+    let age = 14;
+    alert("I am " + age + " years old");
+
+    // Visits counter
     let visits = localStorage.getItem("visitCount");
     if (visits === null) {
       visits = 0;
@@ -9,12 +10,16 @@ git
     visits++;
     localStorage.setItem("visitCount", visits);
     alert("You have visited this site " + visits + " times.");
-    
-let birthyear = 2011
-   document.write("My birth year is " + birthyear + "<br>");
-     document.write("Data type of my declared variable is number" );
-    
+
+    // Birth year
+    let birthyear = 2011;
+    document.getElementById("birth").innerHTML =
+      "My birth year is " + birthyear + "<br>Data type of my declared variable is number";
+
+    // Clothing store
     var visitorName = "John Doe";
     var productTitle = "T-shirt";
     var quantity = 5;
-    document.write(visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store.");
+    document.getElementById("store").innerHTML =
+      visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store.";
+  
